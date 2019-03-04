73154755a59893fc093b4e68ad35b4019432b169
homer0 <leo.apiwan@olapic.com>
2019-03-04 05:42:15 -0300

    Implement the new Menu on the Section layout

--- src/layouts/section/index.jsx
+++ src/layouts/section/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import DefaultLayout from 'layouts/default';
+import { Menu } from 'components/Menu';
 import { Footer } from 'components/Footer';
 import MainLogoImage from 'assets/images/logo-main.svg';
+import MobileLogoImage from 'assets/images/logo-mobile.svg';
 import styles from './styles.module.scss';
 
-const SectionLayout = ({ sectionTitle, seoProps, children }) => {
+const SectionLayout = ({ sectionTitle, seoProps, currentPath, children }) => {
   const defaultLayoutProps = { seoProps };
   defaultLayoutProps.seoProps.subtitle = sectionTitle;
 
@@ -13,13 +16,23 @@ const SectionLayout = ({ sectionTitle, seoProps, children }) => {
       <section className={styles.section}>
         <aside className={styles.left}>
           <div className={styles.logoContainer}>
-            <img
-              className={styles.logo}
-              alt="Logo principal de Webconf edición 2019"
-              src={MainLogoImage}
-            />
+            <Link to="/">
+              <img
+                className={styles.logo}
+                alt="Logo principal de Webconf edición 2019"
+                src={MainLogoImage}
+              />
+              <img
+                className={styles.logoMobile}
+                alt="Logo principal de Webconf edición 2019"
+                src={MobileLogoImage}
+              />
+            </Link>
           </div>
-          <div className={styles.navigationContainer}>Navigation</div>
+          <h1 className={styles.titleMobile}>{sectionTitle}</h1>
+          <div className={styles.navigationContainer}>
+            <Menu currentPath={currentPath} />
+          </div>
         </aside>
         <div className={styles.right}>
           <h1 className={styles.title}>{`{${sectionTitle}}`}</h1>
