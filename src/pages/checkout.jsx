cdb9c6f108507a582807fe23c6e17e7d1d809a89
homer0 <leo.apiwan@olapic.com>
2019-03-04 05:41:51 -0300

    Send the current route path to the layout

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 // Layouts
 import SectionLayout from 'layouts/section';
 
-const CheckoutPage = () => <SectionLayout sectionTitle="Entradas">Checkout</SectionLayout>;
+const CheckoutPage = props => (
+  <SectionLayout sectionTitle="Entradas" currentPath={props.location.pathname}>
+    Checkout
+  </SectionLayout>
+);
 
 export default CheckoutPage;
