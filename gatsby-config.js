e63df1bd47d90eb9c7b9e6814622bab47a455dfe
homer0 <leo.apiwan@olapic.com>
2019-03-08 05:40:55 -0300

    Implement Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,13 @@ module.exports = {
         display: `minimal-ui`,
         icon: path.join(__dirname, `src/assets/images/icon.png`) // This path is relative to the root of the site.
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.ANALYTICS_KEY,
+        cookieDomain: process.env.ANALYTICS_DOMAIN
+      }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
