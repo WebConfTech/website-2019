e0a27b348035113acf40900ff12a3c3d67afa2e4
homer0 <leo.apiwan@olapic.com>
2019-03-10 01:35:49 -0300

    fix(analytics): replace promises with async/await

--- src/common/ga.js
+++ src/common/ga.js
@@ -1,25 +1,23 @@
 import { waitForGlobal } from './utils';
 
 let client;
-const getClient = () => {
+const getClient = async () => {
   if (client) {
     return Promise.resolve(client);
   }
 
-  return waitForGlobal('ga').then(gaClient => {
-    client = gaClient;
-    return client;
-  });
+  const gaClient = await waitForGlobal('ga');
+  client = gaClient;
+  return client;
 };
 
-export const trackEvent = (category, action, label, value) => {
+export const trackEvent = async (category, action, label, value) => {
   if (process.env.NODE_ENV === 'production') {
-    getClient()
-      .then(gaClient => {
-        gaClient('send', 'event', category, action, label, value);
-      })
-      .catch(error => {
-        console.error('Unable to track event:', error.message);
-      });
+    try {
+      const gaClient = await getClient();
+      gaClient('send', 'event', category, action, label, value);
+    } catch (error) {
+      console.error('Unable to track event:', error.message);
+    }
   }
 };
