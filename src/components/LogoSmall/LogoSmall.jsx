ce4a4ac0e7b57d53edf49ff9c040c18b7983ea6d
Agustin Carrasco <asermax@gmail.com>
2019-03-13 21:35:49 -0300

    fix(small logo): correct export and import Link

--- src/components/LogoSmall/LogoSmall.jsx
+++ src/components/LogoSmall/LogoSmall.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Link from 'gatsby-link';
 import MobileLogoImage from 'assets/images/logo-mobile.svg';
 import styles from './styles.module.scss';
 
-const LogoSmall = ({ className = '' }) => (
+export const LogoSmall = ({ className = '' }) => (
   <Link to="/" className={`${styles.container} ${className}`}>
     <img
       className={styles.logo}
