80275fa0e24ade19a42ba5183f9e278738f769aa
Agustin Carrasco <asermax@gmail.com>
2019-03-17 20:31:31 -0300

    refactor(CTALeafLike): tweak to accept position, color and icon

--- src/components/CFPButton/CFPButton.jsx
+++ src/components/CFPButton/CFPButton.jsx
@@ -4,7 +4,9 @@ import { OutboundLink } from 'gatsby-plugin-google-analytics';
 import { CTALeafLike } from 'lib';
 
 export const CFPButton = () => (
-  <CTALeafLike href={CFP.url} title={CFP.title} as={OutboundLink} target="_blank">
-    {CFP.title}
+  <CTALeafLike href={CFP.url} title={CFP.title} as={OutboundLink} color="secondary" target="_blank">
+    ¿Querés dar
+    <br />
+    una charla?
   </CTALeafLike>
 );
