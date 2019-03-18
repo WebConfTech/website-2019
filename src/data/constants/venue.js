dac93016d10c6fefede2c1aa7194b5aebe850aa0
Agustin Carrasco <asermax@gmail.com>
2019-03-18 03:04:15 -0300

    fix(venue): gatsby doesn't like leaflet on some situations

--- src/data/constants/venue.js
+++ src/data/constants/venue.js
@@ -1,11 +1,10 @@
-import L from 'leaflet';
 import MapMarker from 'assets/images/map-marker.svg';
 
 export const VENUE_LAT_LNG = [-31.41941, -64.18688];
 export const VENUE_DEFAULT_ZOOM = 18;
 export const VENUE_MARKER =
   typeof window !== 'undefined'
-    ? new L.Icon({
+    ? new (require('leaflet')).Icon({
         iconUrl: MapMarker,
         iconSize: [45, 72],
         iconAnchor: [22.5, 72]
