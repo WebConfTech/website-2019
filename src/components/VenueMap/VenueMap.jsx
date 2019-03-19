8e045a3945c45da1e9c6f3db87f96f60ea13de6d
Agustin Carrasco <asermax@gmail.com>
2019-03-19 13:12:03 -0300

    refactor(venue): adapt map positioning for mobile

--- src/components/VenueMap/VenueMap.jsx
+++ src/components/VenueMap/VenueMap.jsx
@@ -1,22 +1,16 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Map, TileLayer, Marker } from 'react-leaflet';
-import { VENUE_LAT_LNG, VENUE_DEFAULT_ZOOM, VENUE_MARKER } from 'data/constants';
+import { VENUE_LAT_LNG, VENUE_MARKER } from 'data/constants';
 import styles from './styles.module.scss';
 
-export const VenueMap = () => {
-  const mapRef = useRef(null);
+export const VenueMap = ({ zoom }) => {
   const position = VENUE_LAT_LNG;
-  const zoom = VENUE_DEFAULT_ZOOM;
   const icon = VENUE_MARKER;
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => mapRef.current.leafletElement.invalidateSize(), 1000);
-    return () => clearTimeout(timeoutId);
-  }, [mapRef]);
 
   return typeof window !== 'undefined' ? (
     <>
-      <Map center={position} zoom={zoom} className={styles.map} ref={mapRef}>
+      <Map center={position} zoom={zoom} className={styles.map}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -25,4 +19,8 @@ export const VenueMap = () => {
       </Map>
     </>
   ) : null;
+};
+
+VenueMap.propTypes = {
+  zoom: PropTypes.number.isRequired
 };
--- src/data/constants/index.js
+++ src/data/constants/index.js
@@ -3,4 +3,9 @@ export { SEO_DATA } from './seoData';
 export { CFP } from './cfp';
 export { MENU } from './menu';
 export { EVENT_DATE } from './event';
-export { VENUE_LAT_LNG, VENUE_DEFAULT_ZOOM, VENUE_MARKER } from './venue';
+export {
+  VENUE_LAT_LNG,
+  VENUE_DESKTOP_DEFAULT_ZOOM,
+  VENUE_MOBILE_DEFAULT_ZOOM,
+  VENUE_MARKER
+} from './venue';
