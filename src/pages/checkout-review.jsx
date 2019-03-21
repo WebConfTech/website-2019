749ae87ebb2d5f541921a171d19e084aee561bd1
homer0 <leo.apiwan@olapic.com>
2019-03-21 03:21:14 -0300

    feat(checkout): add page to review the checkout payment

---
+++ src/pages/checkout-review.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { CheckoutMenu } from 'components/Menu';
+import SectionLayout, { SectionTitle } from 'layouts/section';
+import { PaymentReviewCard } from 'components/PaymentReviewCard';
+import styles from './checkout-review.module.scss';
+
+const CheckoutReviewPage = props => (
+  <SectionLayout
+    sectionTitle="Entradas"
+    currentPath={props.location.pathname}
+    className={styles.section}
+    menuComponent={() => <CheckoutMenu short />}
+  >
+    <SectionTitle>Entradas</SectionTitle>
+    <div className={styles.reviewContainer}>
+      <PaymentReviewCard />
+    </div>
+  </SectionLayout>
+);
+
+export default CheckoutReviewPage;
