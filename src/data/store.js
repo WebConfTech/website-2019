9a87d2ee5555ebf1fc8bbf20d8febeb2a80950c5
Agustin Carrasco <asermax@gmail.com>
2019-03-22 08:14:19 -0300

    feat(checkout): save state on local storage

--- src/data/store.js
+++ src/data/store.js
@@ -1,16 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { save, load } from 'redux-localstorage-simple';
 import appReducers from './reducers';
 
-export function configureStore(initialState = {}) {
+export function configureStore() {
+  // localstare configuration
+  const localStorageConf = {
+    namespace: 'webconf',
+    states: ['checkout'],
+    debounce: 500
+  };
+
   // create store
   const composeEnhancers =
-    (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-    compose;
+    (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
   const store = createStore(
     appReducers,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk))
+    load(localStorageConf),
+    composeEnhancers(applyMiddleware(thunk, save(localStorageConf)))
   );
 
   return store;
