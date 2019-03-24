fde1c2be021f31f08d360783517c6dedc212b825
Agustin Carrasco <asermax@gmail.com>
2019-03-24 18:12:03 -0300

    feat(api): add get helper and include the customer API url

--- src/common/api.js
+++ src/common/api.js
@@ -1,6 +1,7 @@
-import { API_BASE_URL } from './config';
+import { API_BASE_URL, CHECKOUT_BASE_URL } from './config';
 
 export const SAVE_EMAIL = `${API_BASE_URL}/lambdas/mail_save`;
+export const CUSTOMER = `${CHECKOUT_BASE_URL}/api/customers`;
 
 const makeHeaders = () =>
   new Headers({
@@ -8,12 +9,20 @@ const makeHeaders = () =>
     'Content-Type': 'application/json'
   });
 
-export const post = async (url, data) => {
-  const response = await fetch(url, {
+export const get = (url, parameters) => {
+  const urlObject = new URL(url);
+
+  if (parameters) {
+    Object.keys(parameters).forEach(key => urlObject.searchParams.append(key, parameters[key]));
+  }
+
+  return fetch(urlObject.toString());
+};
+
+export const post = (url, data) => {
+  return fetch(url, {
     headers: makeHeaders(),
     method: 'POST',
     body: JSON.stringify(data)
   });
-
-  return response;
 };
