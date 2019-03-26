1bba570bbb7e0ec4021eb9437042fbe9cdf6495d
Agustin Carrasco <asermax@gmail.com>
2019-03-26 11:07:21 -0300

    fix(checkout): don't clear tickets on error

--- src/pages/checkout/error.jsx
+++ src/pages/checkout/error.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { clearTickets } from 'data/checkout/actions';
+import React from 'react';
 import Link from 'gatsby-link';
 import SectionLayout, { SectionTitle } from 'layouts/section';
 import { Button } from 'lib/Button';
@@ -11,67 +9,58 @@ import purchaseErrorImage from 'assets/images/purchase-error.svg';
 const CHECKOUT_MENU = [
   {
     title: 'Entradas',
-    enabled: true,
-    url: '/checkout'
+    url: '/checkout/',
+    enabled: true
   },
   {
     title: 'Revisá tu compra',
-    enabled: false
+    url: '/checkout/review/',
+    enabled: true
   },
   {
-    title: '¡Listo!',
-    enabled: false
+    title: 'Ocurrió un problema',
+    url: '/checkout/error/',
+    enabled: true
   }
 ];
 
-const CheckoutPendingPage = ({ clear }) => {
-  useEffect(clear, []);
-
-  return (
-    <SectionLayout
-      title="Error"
-      className={styles.section}
-      menuComponent={() => <CheckoutMenu items={CHECKOUT_MENU} short hideOnMobile />}
-      hideFooterOnMobile
-      hideSidebarOnMobile
-    >
-      <SectionTitle>Entradas</SectionTitle>
-      <div className={styles.container}>
-        <h2 className={styles.title}>Uff...</h2>
-        <div className={styles.imageContainer}>
-          <img src={purchaseErrorImage} className={styles.image} alt="Error" />
-        </div>
-        <div className={styles.textContainer}>
-          <p className={styles.text}>
-            <strong>Algo salió mal.</strong>
-          </p>
-          <p className={styles.text}>
-            Puede que haya algún
-            <br />
-            problema con la forma
-            <br />
-            de pago que elegiste.
-          </p>
-          <p className={styles.text}>
-            Elegí otra forma de pago
-            <br />e intentá nuevamente.
-          </p>
-          <p className={styles.text}>
-            <Button as={Link} className={styles.button} to="/checkout" large>
-              Volver al principio
-            </Button>
-          </p>
-        </div>
+const CheckoutPendingPage = ({ clear }) => (
+  <SectionLayout
+    title="Error"
+    className={styles.section}
+    menuComponent={() => <CheckoutMenu items={CHECKOUT_MENU} short hideOnMobile />}
+    hideFooterOnMobile
+    hideSidebarOnMobile
+  >
+    <SectionTitle>Entradas</SectionTitle>
+    <div className={styles.container}>
+      <h2 className={styles.title}>Uff...</h2>
+      <div className={styles.imageContainer}>
+        <img src={purchaseErrorImage} className={styles.image} alt="Error" />
       </div>
-    </SectionLayout>
-  );
-};
+      <div className={styles.textContainer}>
+        <p className={styles.text}>
+          <strong>Algo salió mal.</strong>
+        </p>
+        <p className={styles.text}>
+          Puede que haya algún
+          <br />
+          problema con la forma
+          <br />
+          de pago que elegiste.
+        </p>
+        <p className={styles.text}>
+          Elegí otra forma de pago
+          <br />e intentá nuevamente.
+        </p>
+        <p className={styles.text}>
+          <Button as={Link} className={styles.button} to="/checkout/review/" large>
+            Volver al principio
+          </Button>
+        </p>
+      </div>
+    </div>
+  </SectionLayout>
+);
 
-const mapDispatchToProps = dispatch => ({
-  clear: () => dispatch(clearTickets())
-});
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(CheckoutPendingPage);
+export default CheckoutPendingPage;
