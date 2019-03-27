8250418af89bf18763c7861639a9be48d8f2695d
Agustin Carrasco <asermax@gmail.com>
2019-03-27 15:19:26 -0300

    chore(speakers): boilerplate

---
+++ src/pages/speakers.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SectionLayout, { SectionTitle } from 'layouts/section';
+
+const SpeakersPage = () => (
+  <SectionLayout title="disertantes">
+    <SectionTitle>Disertantes</SectionTitle>
+  </SectionLayout>
+);
+
+export default SpeakersPage;
