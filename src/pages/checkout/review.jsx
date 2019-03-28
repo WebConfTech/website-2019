a26ba25b908a614ef386d3ccada3db3f9290dac5
Agustin Carrasco <asermax@gmail.com>
2019-03-28 01:09:46 -0300

    fix(checkout/review): don't allow to go back to tickets if purchase exists

--- src/pages/checkout/review.jsx
+++ src/pages/checkout/review.jsx
@@ -1,4 +1,7 @@
+import * as R from 'ramda';
 import React from 'react';
+import { connect } from 'react-redux';
+import { isPurchaseCreated } from 'data/checkout/selectors';
 import { CheckoutMenu } from 'components/Menu';
 import SectionLayout, { SectionTitle } from 'layouts/section';
 import { PaymentReviewCard } from 'components/PaymentReviewCard';
@@ -21,19 +24,32 @@ const CHECKOUT_MENU = [
   }
 ];
 
-const CheckoutReviewPage = ({ alreadyInitiated }) => (
-  <SectionLayout
-    title="Revisá tu compra"
-    className={styles.section}
-    menuComponent={() => <CheckoutMenu items={CHECKOUT_MENU} short hideOnMobile />}
-    mobileBackButtonRoute="/checkout/"
-    hideFooterOnMobile
-  >
-    <SectionTitle>Entradas</SectionTitle>
-    <div className={styles.reviewContainer}>
-      <PaymentReviewCard />
-    </div>
-  </SectionLayout>
-);
+const CheckoutReviewPage = ({ alreadyInitiated }) => {
+  const menu = R.adjust(
+    0,
+    R.evolve({
+      enabled: R.always(!alreadyInitiated)
+    })
+  )(CHECKOUT_MENU);
 
-export default CheckoutReviewPage;
+  return (
+    <SectionLayout
+      title="Revisá tu compra"
+      className={styles.section}
+      menuComponent={() => <CheckoutMenu items={menu} short hideOnMobile />}
+      mobileBackButtonRoute={alreadyInitiated ? null : '/checkout/'}
+      hideFooterOnMobile
+    >
+      <SectionTitle>Entradas</SectionTitle>
+      <div className={styles.reviewContainer}>
+        <PaymentReviewCard />
+      </div>
+    </SectionLayout>
+  );
+};
+
+const mapStateToProps = state => ({
+  alreadyInitiated: isPurchaseCreated(state)
+});
+
+export default connect(mapStateToProps)(CheckoutReviewPage);
