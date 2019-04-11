7aa2557f416cedcd29ac00871bb5147e40db4341
Agustin Carrasco <asermax@gmail.com>
2019-04-10 23:05:35 -0300

    feat(speakers): placeholder for speakers

--- src/pages/speakers.jsx
+++ src/pages/speakers.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import Photo from 'assets/images/banner-background-mobile.png';
 import SectionLayout, { SectionTitle } from 'layouts/section';
+import { SpeakerGrid, Speaker } from 'lib';
+import styles from './speakers.module.scss';
 
 const SpeakersPage = () => (
   <SectionLayout title="disertantes">
     <SectionTitle>Disertantes</SectionTitle>
+    <SpeakerGrid className={styles.speakersGrid}>
+      <Speaker
+        firstName="Agus"
+        lastName="Carrasco"
+        image={Photo}
+        description="test description"
+        twitterHandle="asermax"
+      />
+      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
+      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
+      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
+      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
+      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
+      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
+      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
+    </SpeakerGrid>
   </SectionLayout>
 );
 
