0994dd20552ef199ac9a956953d8c616cafcf67f
Agustin Carrasco <asermax@gmail.com>
2019-04-13 00:23:13 -0300

    fix(speakers): husky breaks stuff

--- lib/SpeakerGrid/SpeakerGrid.jsx
+++ lib/SpeakerGrid/SpeakerGrid.jsx
@@ -17,23 +17,23 @@ export const SpeakerGrid = styled.div`
     }
   }
 
-  ${SpeakerContainer}:nth-of-type (even) {
+  ${SpeakerContainer}:nth-of-type(even) {
     flex-direction: column-reverse;
   }
 
-  ${SpeakerContainer}:nth-of-type (3n + 1) {
+  ${SpeakerContainer}:nth-of-type(3n + 1) {
     ${SpeakerName} {
       background-color: ${({ theme }) => theme.palette.secondary};
     }
   }
 
-  ${SpeakerContainer}:nth-of-type (3n + 2) {
+  ${SpeakerContainer}:nth-of-type(3n + 2) {
     ${SpeakerName} {
       background-color: ${({ theme }) => theme.palette.generics.black};
     }
   }
 
-  ${SpeakerContainer}:nth-of-type (3n + 3) {
+  ${SpeakerContainer}:nth-of-type(3n + 3) {
     ${SpeakerName} {
       background-color: ${({ theme }) => theme.palette.primary};
     }
