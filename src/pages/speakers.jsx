de76d9f8c176767d76735bf020b3449047d3c207
Agustin Carrasco <asermax@gmail.com>
2019-04-12 23:18:34 -0300

    fix(speakers): add newsletter and CTA

--- src/pages/speakers.jsx
+++ src/pages/speakers.jsx
@@ -5,7 +5,7 @@ import { SPEAKERS } from 'data/constants';
 import styles from './speakers.module.scss';
 
 const SpeakersPage = () => (
-  <SectionLayout title="disertantes" className={styles.section}>
+  <SectionLayout title="disertantes" className={styles.section} newsletter cta>
     <SectionTitle>Disertantes</SectionTitle>
     <SpeakerGrid className={styles.grid}>
       {SPEAKERS.map((speaker, index) => (
