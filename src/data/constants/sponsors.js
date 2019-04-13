8859ae1f2588aba0fe131570963cd9d144a86122
homer0 <leo.apiwan@olapic.com>
2019-04-13 06:13:07 -0300

    feat(sponsors): add constnat with the sponsors information

---
+++ src/data/constants/sponsors.js
@@ -0,0 +1,43 @@
+export const SPONSORS = {
+  gold: [
+    {
+      title: 'Virtualmind',
+      image: require('assets/images/sponsors/gold/virtualmind.png'),
+      url: 'https://virtualmind.com'
+    },
+    {
+      title: 'Naranja',
+      image: require('assets/images/sponsors/gold/naranja.png'),
+      url: 'https://naranja.com'
+    },
+    {
+      title: 'Olapic',
+      image: require('assets/images/sponsors/gold/olapic.svg'),
+      url: 'https://olapic.com'
+    }
+  ],
+  silver: [
+    {
+      title: 'Mercado Libre',
+      image: require('assets/images/sponsors/silver/mercado-libre.png'),
+      url: 'https://mercadolibre.com.ar'
+    }
+  ],
+  bronze: [
+    {
+      title: 'Sentry',
+      image: require('assets/images/sponsors/bronze/sentry.png'),
+      url: 'https://sentry.io'
+    },
+    {
+      title: 'Pasionaria Delicatessen',
+      image: require('assets/images/sponsors/bronze/pasionaria.png'),
+      url: 'https://fb.com/pasionariadelicatessen'
+    },
+    {
+      title: 'Estudio jurídico Eroles - Saurralles & asociados',
+      image: require('assets/images/sponsors/bronze/eroles-saurralles.png'),
+      url: 'https://google.com'
+    }
+  ]
+};
