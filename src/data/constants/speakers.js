05262939b00bec13b692f081493b016b595c3110
Agustin Carrasco <asermax@gmail.com>
2019-04-12 21:59:42 -0300

    feat(speakers): add current speakers data

---
+++ src/data/constants/speakers.js
@@ -0,0 +1,133 @@
+export const SPEAKERS = [
+  {
+    firstName: 'Pedro Sonzini',
+    lastName: 'Astudillo',
+    image: require('assets/images/speakers/pedro-sonzini-astudillo.jpg'),
+    shortDescription: `
+Pedro es Frontend Developer, emprendedor y administrador de su propio espacio Coworking en
+Villa Allende.
+
+En **Una historia de layouts** nos invita a recorrer la historia desde el punto de
+vista del desarrollador a la hora de implementar una nueva estructura o layout.
+    `,
+    longDescription: `
+Pedro es Frontend Developer, emprendedor y administrador de su propio espacio Coworking en
+Villa Allende.
+
+En **Una historia de layouts** nos invita a recorrer la historia desde el punto de
+vista del desarrollador a la hora de implementar una nueva estructura o layout. Nos va a
+presentar las herramientas que tenemos, las que se vienen y las que marcan el futuro.
+    `
+  },
+  {
+    firstName: 'Gonzalo',
+    lastName: 'Pozzo',
+    image: require('assets/images/speakers/gonzalo-pozzo.jpg'),
+    shortDescription: `
+Gonzalo es un React developer en The Next Ad, co-organizador en freeCodeCampBA.
+
+En **GG Easy con Cypress, testeando una app entera en media hora (no clickbait)** nos va a
+explicar cuando y por que hacer tests de integración o unitarios, y qué ventajas tiene Cypress
+sobre Selenium o Puppeteer.
+    `,
+    longDescription: `
+Gonzalo es un React developer en The Next Ad, co-organizador en freeCodeCampBA, al que le gusta
+mentorear, ayudar y los perritos.
+
+En **GG Easy con Cypress, testeando una app entera en media hora (no clickbait)** nos va a explicar
+cuando y por que hacer tests de integración o unitarios, y qué ventajas tiene Cypress sobre
+Selenium o Puppeteer.
+    `
+  },
+  {
+    firstName: 'Alex',
+    lastName: 'Saez',
+    image: require('assets/images/speakers/alex-saez.jpg'),
+    shortDescription: `
+Alex es Fullstack Developer, con más de 8 años de experiencia.
+
+En **Componentes web reutilizables e independientes del framework** nos invita a dejar de lado la
+guerra de frameworks y ver cómo implementar componentes reutilizables con Web Components.
+    `,
+    longDescription: `
+Alex es Fullstack Developer especializado en y fanático del Frontend, con más de 8 años de
+experiencia. Además escribe en la página ProTecno.io
+
+En **Componentes web reutilizables e independientes del framework** nos invita a dejar de lado
+la guerra de frameworks y ver cómo implementar componentes reutilizables en casi cualquiera de
+ellos con Web Components.
+`
+  },
+  {
+    firstName: 'Santiago',
+    lastName: 'Barchetta',
+    image: require('assets/images/speakers/santiago-barchetta.jpg'),
+    shortDescription: `
+Santiago trabaja con la web desde hace más de 10 años y hace 5 que se desempeña como Frontend
+Engineer en Mercado Libre.
+
+En **El mejor amigo para tu frontend - BFF** compartirá algunos aprendizajes obtenidos en la
+implementación del patrón BFF (Backend for Frontend).
+`,
+    longDescription: `
+Santiago trabaja con la web desde hace más de 10 años y hace 5 que se desempeña como Frontend
+Engineer en Mercado Libre.
+
+En **El mejor amigo para tu frontend - BFF** compartirá algunos aprendizajes obtenidos en la
+implementación del patrón BFF (Backend for Frontend). Se adentrará en los desafíos, decisiones
+importantes y tips a tener en cuenta.
+    `
+  },
+  {
+    firstName: 'Joel Villareal',
+    lastName: 'Bertoldi',
+    image: require('assets/images/speakers/joel-villarreal-bertoldi.jpg'),
+    shortDescription: `
+Joel escribe JavaScript desde hace 20 años y actualmente trabaja como Software Engineer en
+Counterfactual.
+
+En **<Crecer Con=”La Web” />** nos va a contar una historia sobre la infancia y adolescencia del
+front-end, intentando explicar cómo este ámbito del desarrollo Web llegó a ser lo que es hoy.
+    `,
+    longDescription: `
+Joel escribe JavaScript desde hace casi 20 años y actualmente trabaja como Software Engineer en
+Counterfactual. Amante de los videojuegos, la música, la ciencia ficción y los mundos de fantasía.
+
+En **<Crecer Con=”La Web” />** nos va a contar una historia sobre la infancia y adolescencia del
+front-end, intentando explicar cómo este ámbito del desarrollo Web llegó a ser lo que es hoy.
+`
+  },
+  {
+    firstName: 'Nabila',
+    lastName: 'Nahir Gudiño ',
+    image: require('assets/images/speakers/nabila-gudino.jpg'),
+    shortDescription: `
+Nabila es Ingeniera por elección, diseñadora por vocación. Le gusta mucho lo que hace, sobre todo
+el fino arte de entender a los usuarios.
+
+En **Diseñando el código** nos va a contar que son los design systems, en qué consiste una style guide y como armar una component library.
+    `,
+    longDescription: `
+Nabila es Ingeniera por elección, diseñadora por vocación. Le gusta mucho lo que hace, sobre todo
+el fino arte de entender a los usuarios.
+
+En **Diseñando el código** nos va a contar que son los design systems, en qué consiste una style
+guide y como armar una component library, explorando como diseñadores y desarrolladores trabajan
+en conjunto.
+`
+  },
+  {
+    firstName: 'TBA',
+    lastName: '',
+    image: require('assets/images/banner-background-mobile.png'),
+    shortDescription: 'TBA',
+    longDescription: 'TBA'
+  },
+  {
+    firstName: 'TBA',
+    lastName: '',
+    image: require('assets/images/banner-background-mobile.png'),
+    shortDescription: 'TBA',
+    longDescription: 'TBA'
+  }
+];
--- src/pages/speakers.jsx
+++ src/pages/speakers.jsx
@@ -2,26 +2,16 @@ import React from 'react';
 import Photo from 'assets/images/banner-background-mobile.png';
 import SectionLayout, { SectionTitle } from 'layouts/section';
 import { SpeakerGrid, Speaker } from 'lib';
+import { SPEAKERS } from 'data/constants';
 import styles from './speakers.module.scss';
 
 const SpeakersPage = () => (
   <SectionLayout title="disertantes" className={styles.section}>
     <SectionTitle>Disertantes</SectionTitle>
     <SpeakerGrid className={styles.grid}>
-      <Speaker
-        firstName="Agus"
-        lastName="Carrasco"
-        image={Photo}
-        description="test description"
-        twitterHandle="asermax"
-      />
-      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
-      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
-      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
-      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
-      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
-      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
-      <Speaker firstName="Agus" lastName="Carrasco" image={Photo} description="test description" />
+      {SPEAKERS.map((speaker, index) => (
+        <Speaker key={index} {...speaker} />
+      ))}
     </SpeakerGrid>
   </SectionLayout>
 );
