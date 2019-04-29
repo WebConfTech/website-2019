ada3f4f9d99144abfc0b576eedaa025183cec57d
homer0 <leo.apiwan@olapic.com>
2019-04-29 05:23:11 -0300

    fix(components/Schedule): add a max width for big screens

--- src/components/Schedule/ScheduleList.jsx
+++ src/components/Schedule/ScheduleList.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-export const ScheduleList = ({ children }) => <ul className={styles.list}>{children}</ul>;
+export const ScheduleList = ({ children }) => (
+  <div className={styles.listContainer}>
+    <ul className={styles.list}>{children}</ul>
+  </div>
+);
