a74dd44db757a254f1c17ed06aa37f8db5b64272
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-04-29 06:00:43 -0300

    Update src/data/constants/speakers.js
    Co-Authored-By: homer0 <6949310+homer0@users.noreply.github.com>

--- src/data/constants/speakers.js
+++ src/data/constants/speakers.js
@@ -149,7 +149,7 @@ guide y como armar una component library, explorando como diseñadores y desarro
 en conjunto.
     `,
     scheduleDescription: `
-Veremos que son los design systems, en qué consiste una style guide y como armar una component library, explorando como diseñadores y desarrolladores trabajan en conjunto.
+Veremos qué son los design systems, en qué consiste una style guide y cómo armar una component library, explorando la manera en que diseñadores y desarrolladores trabajan en conjunto.
     `
   },
   {
