69d7ff1442f544d9aa89fc8578b6e1346f9c9905
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-04-29 06:00:55 -0300

    Update src/data/constants/speakers.js
    Co-Authored-By: homer0 <6949310+homer0@users.noreply.github.com>

--- src/data/constants/speakers.js
+++ src/data/constants/speakers.js
@@ -191,7 +191,7 @@ Pablo es Ingeniero en Sistemas y trabaja hace más de 10 años en diversas área
 En su charla **Irrepresentabilidad de estados inválidos** en JavaScript nos va a contar acerca de las fortalezas del tipado estático y cómo su implementación contribuye a una mejora en la calidad del código.
     `,
     scheduleDescription: `
-Cuales son las fortalezas del tipado estático y cómo su implementación contribuye a una mejora en la calidad del código
+Las fortalezas del tipado estático y cómo su implementación contribuye a una mejora en la calidad del código y, con un poco de suerte, a una reducción en la cantidad de errores en producción.
     `
   },
   {
