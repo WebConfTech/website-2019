1c37ae79c5f6f739b34d7d5812aefa1f226c09c0
Agustin Carrasco <asermax@gmail.com>
2019-05-29 23:40:22 -0300

    fix(landing): fix some props

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,16 +7,16 @@ import styles from './index.module.scss';
 
 const IndexPage = () => (
   <LandingLayout>
-    <div class={styles.videoBackground}>
-      <div class={styles.videoForeground}>
+    <div className={styles.videoBackground}>
+      <div className={styles.videoForeground}>
         <iframe
           title="resumen"
-          frameborder="0"
+          frameBorder="0"
           height="100%"
           width="100%"
           src="https://youtube.com/embed/h6sbrtMnMzM?version=3&autoplay=1&loop=1&controls=1&playlist=h6sbrtMnMzM"
           allow="autoplay; encrypted-media"
-          allowfullscreen
+          allowFullScreen
         />
       </div>
     </div>
