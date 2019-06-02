18303562beb78ebbcd53e88168d746f85bdd2db8
Agustin Carrasco <asermax@gmail.com>
2019-06-01 23:55:32 -0300

    fix(useWindowWidth): static build doesn't have window

--- src/common/hooks/useWindowWidth.js
+++ src/common/hooks/useWindowWidth.js
@@ -1,13 +1,18 @@
 import { useState, useEffect } from 'react';
+import theme from 'lib/theme';
 
 export const useWindowWidth = () => {
-  const [width, setWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(
+    typeof window !== 'undefined' ? window.innerWidth : theme.breakpoints.smallScreen
+  );
 
   useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener('resize', handleResize);
+    if (typeof window !== 'undefined') {
+      const handleResize = () => setWidth(window.innerWidth);
+      window.addEventListener('resize', handleResize);
 
-    return () => window.removeEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }
   });
 
   return width;
