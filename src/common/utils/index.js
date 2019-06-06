3c7b2780e5556f7ba9576c1f89cab4179f3e63c3
homer0 <leo.apiwan@olapic.com>
2019-06-06 02:46:25 -0300

    feat(common/utils): add service to interact with the YouTube API

--- src/common/utils/index.js
+++ src/common/utils/index.js
@@ -1,3 +1,4 @@
 export { deferred } from './deferred';
 export { maskDni, unmaskDni } from './dniMask';
 export { waitForGlobal } from './waitForGlobal';
+export { getYouTubeAPI, loadYouTubeVideo } from './youtube';
